64066395847efa5496e7e660821379e6705d1def
George Ho <georgeho1618@gmail.com>
2018-04-25 20:01:15 -0400

    ENH added refresh script

---
+++ refresh.sh
@@ -0,0 +1,7 @@
+# helper script to call train.py
+# python train.py --corpus=lyre --learning_rate=1e-3 --new_init=False --loss_function=sc &
+
+cd mini_canne/
+python train.py --corpus=lyre --learning_rate=1e-3 --new_init=True --loss_function=sc &
+sleep 10
+pkill python
