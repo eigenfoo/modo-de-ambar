cbc43dcc5fc3a02c3a02361645a7f48913aec5c3
George Ho <georgeho1618@gmail.com>
2018-05-01 00:40:26 -0400

    DOC added license to source files

--- shell_scripts/call_train.sh
+++ shell_scripts/call_train.sh
@@ -1,5 +1,17 @@
-# helper script to call train.py
-# python train.py --corpus=lyre --learning_rate=1e-3 --new_init=False --loss_function=sc &
+# Copyright 2018 Ella de Buck, Joseph Colonel, George Ho, Richard Yee
+# 
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+# 
+#     http://www.apache.org/licenses/LICENSE-2.0
+# 
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 
+# Helper script to call train.py
 cd mini_canne/
 python train.py --corpus=$1 --learning_rate=$2 --new_init=$3 --loss_function=$4
--- shell_scripts/kill_generate.sh
+++ shell_scripts/kill_generate.sh
@@ -1,3 +1,16 @@
-# helper script to kill generate.py
+# Copyright 2018 Ella de Buck, Joseph Colonel, George Ho, Richard Yee
+# 
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+# 
+#     http://www.apache.org/licenses/LICENSE-2.0
+# 
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 
+# Helper script to kill generate.py
 pgrep -f 'python generate.py' | xargs kill -9
